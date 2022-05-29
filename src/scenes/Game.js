f8d38bba92343c73e797fd365393812bfe50acec
Sean Gravener <sean@gravener.net>
2022-05-29 19:02:37 -0400

    Add hero

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -1,20 +1,25 @@
-import Phaser from 'phaser';
+import Phaser from "phaser";
 
+// game object factory
 class Game extends Phaser.Scene {
   constructor() {
-    super({ key: 'GameScene' });
+    super({ key: "GameScene" });
   }
 
   init(data) {}
 
   preload() {
-    this.load.image('logo', 'assets/phaser3-logo.png');
+    this.load.spritesheet("hero-run-sheet", "assets/hero/run.png", {
+      frameWidth: 32,
+      frameHeight: 64,
+    });
   }
 
   create(data) {
-    this.add.image(400, 300, 'logo');
+    this.add.sprite(400, 300, "hero-run-sheet", 3);
   }
 
+  // game loop
   update(time, delta) {}
 }
 
