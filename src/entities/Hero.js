1c52a8e733e8bfaed1a284f69844838a70b3b8a3
Sean Gravener <sean@gravener.net>
2022-05-30 13:18:08 -0400

    Add double jump

--- src/entities/Hero.js
+++ src/entities/Hero.js
@@ -11,27 +11,53 @@ class Hero extends Phaser.GameObjects.Sprite {
 
     // set collision rectangle size and offset
     this.body.setCollideWorldBounds(true).setSize(12, 40).setOffset(12, 23);
+    this.body.setMaxVelocity(250, 400);
+    this.body.setDragX(750);
 
     this.keys = scene.cursorKeys;
   }
 
+  // Update animations
   preUpdate(time, delta) {
     super.preUpdate(time, delta);
 
     if (this.keys.left.isDown) {
-      this.body.setVelocityX(-250);
+      this.body.setAccelerationX(-1000);
       this.setFlipX(true);
-      
+
       // Adjust offset of collision rectangle:
       // [total width of Sprite] - [Width of collision rectangle] - [orig width of offset] = offset.x
       this.body.offset.x = 8;
-    
-    } else if (this.keys.right.isDown) {
-      this.body.setVelocityX(250);
+    }
+
+    // Right Key
+    else if (this.keys.right.isDown) {
+      this.body.setAccelerationX(1000);
       this.setFlipX(false);
+
+      // Reset collision offset
       this.body.offset.x = 12;
-    } else {
-      this.body.setVelocityX(0);
+    }
+
+    // No Key
+    else {
+      // this.body.setVelocityX(0);
+      this.body.setAccelerationX(0);
+    }
+
+    const didPressJump = Phaser.Input.Keyboard.JustDown(this.keys.up);
+    if (didPressJump) {
+      if (this.body.onFloor()) {
+        this.candDoubleJump = true;
+        this.body.setVelocityY(-400);
+      } else if (this.candDoubleJump) {
+        this.candDoubleJump = false;
+        this.body.setVelocityY(-300);
+      }
+    }
+
+    if (!this.keys.up.isDown && this.body.velocity.y < -150) {
+      this.body.velocity.y = -150;
     }
   }
 }
