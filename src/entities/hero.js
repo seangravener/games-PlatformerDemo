f9eb344cd7109ba48fa6c4ffc371d5808920fb6f
Sean Gravener <sean@gravener.net>
2022-08-04 14:26:35 -0400

    Add left\right movement

--- src/entities/hero.js
+++ src/entities/hero.js
@@ -3,8 +3,8 @@ import { GameObjects } from "phaser";
 class Hero extends GameObjects.Sprite {
   constructor(scene, x, y) {
     super(scene, x, y, "hero-run_png");
-    scene.add.existing(this);
-    scene.physics.add.existing(this);
+    this.addtoScene(scene);
+    this.keys = scene.cursorKeys;
 
     // Create .player reference to sprite game object
     // this.player = scene.add.sprite(400, 300, "hero-run_png");
@@ -19,6 +19,27 @@ class Hero extends GameObjects.Sprite {
     // this.add.sprite(400, 400, "hero-run_png"); // as animation
     // this.add.sprite(424, 400, "hero-run_png", 2);
     // this.add.sprite(448, 400, "hero-run_png", 3);
+  }
+
+  addtoScene(scene) {
+    scene.add.existing(this);
+    scene.physics.add.existing(this);
+  }
+
+  preUpdate(...args) {
+    super.preUpdate(...args);
+    const vel = {
+      left: -250,
+      right: 250,
+    };
+
+    if (this.keys.left.isDown) {
+      this.body.setVelocityX(vel.left);
+    } else if (this.keys.right.isDown) {
+      this.body.setVelocityX(vel.right);
+    } else {
+      this.body.setVelocityX(0);
+    }
   }
 }
 
