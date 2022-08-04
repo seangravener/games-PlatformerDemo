bff70ba1ff58991d564a58d76943892b13df94eb
Sean Gravener <sean@gravener.net>
2022-08-04 16:31:27 -0400

    Refactor

--- src/entities/hero.js
+++ src/entities/hero.js
@@ -1,47 +1,74 @@
 import { GameObjects } from "phaser";
 
+const baseUnit = 250;
+const _velocity = {
+  walk: baseUnit / 1.6,
+  run: baseUnit,
+  left: baseUnit * -1,
+  right: baseUnit,
+  drag: 400,
+  accelRate: baseUnit * 4,
+  max: { x: baseUnit, y: baseUnit * 2 },
+};
 class Hero extends GameObjects.Sprite {
   constructor(scene, x, y) {
     super(scene, x, y, "hero-run_png");
-    this.addtoScene(scene);
-    this.keys = scene.cursorKeys;
+    this.init(scene);
 
-    // Create .player reference to sprite game object
-    // this.player = scene.add.sprite(400, 300, "hero-run_png");
-    // Use this.physics to add sprite with physics body
-    this.body.setCollideWorldBounds(true);
-    this.body.setSize(12, 40); // w, h
-    this.body.setOffset(12, 23);
     this.anims.play("hero-running");
-
-    // // Add sprite frame to scene
-    // scene.add.image(100, 100, "hero-run_png"); // as static frame
-    // this.add.sprite(400, 400, "hero-run_png"); // as animation
-    // this.add.sprite(424, 400, "hero-run_png", 2);
-    // this.add.sprite(448, 400, "hero-run_png", 3);
   }
 
-  addtoScene(scene) {
-    scene.add.existing(this);
-    scene.physics.add.existing(this);
+  init(scene) {
+    this.scene = scene;
+
+    this.initScene();
+    this.initPhysicsBody();
+    this.initCursorKeys();
+  }
+
+  initPhysicsBody() {
+    // Set this sprite-body to interact with other objects
+    this.body.setCollideWorldBounds(true);
+
+    // Center this sprite-body collision boundery with offsets
+    this.body.setSize(12, 40); // w, h
+    this.body.setOffset(12, 23);
+
+    // Set movement velocity limits
+    const { drag, max } = _velocity;
+    this.body.setMaxVelocity(max.x, max.y);
+    // this.body.setDragX(drag);
+  }
+
+  initScene() {
+    this.scene.add.existing(this);
+    this.scene.physics.add.existing(this);
+  }
+
+  initCursorKeys() {
+    this.cursorKeys = this.scene.cursorKeys;
   }
 
   preUpdate(...args) {
     super.preUpdate(...args);
-    const vel = {
-      left: -250,
-      right: 250,
-    };
 
-    if (this.keys.left.isDown) {
-      this.body.setVelocityX(vel.left);
+    if (this.cursorKeys.left.isDown) {
+      // this.body.setVelocityX(_velocity.left);
+      this.body.setAccelerationX(_velocity.accelRate * -1);
+      this.body.offset.x = 8; // Center collision boundry with offset
       this.setFlipX(true);
-      this.body.offset.x = 8;
-    } else if (this.keys.right.isDown) {
-      this.body.setVelocityX(vel.right);
+    }
+
+    //
+    else if (this.cursorKeys.right.isDown) {
+      // this.body.setVelocityX(_velocity.right);
+      this.body.setAccelerationX(_velocity.accelRate);
+      this.body.offset.x = 12; // Center collision boundry with offset
       this.setFlipX(false);
-      this.body.offset.x = 12;
-    } else {
+    }
+
+    //
+    else {
       this.body.setVelocityX(0);
     }
   }
