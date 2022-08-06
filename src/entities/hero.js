2d44d98e9750c04495f98eff65634418fd44c642
Sean Gravener <sean@gravener.net>
2022-08-06 04:27:47 -0400

    Add jumping

--- src/entities/hero.js
+++ src/entities/hero.js
@@ -1,4 +1,4 @@
-import { GameObjects } from "phaser";
+import { GameObjects, Input } from "phaser";
 
 const baseUnit = 250;
 const _velocity = {
@@ -6,8 +6,10 @@ const _velocity = {
   run: baseUnit,
   left: baseUnit * -1,
   right: baseUnit,
+  jump: 400,
   drag: 400,
   accelRate: baseUnit * 4,
+  decelRate: 150,
   max: { x: baseUnit, y: baseUnit * 2 },
 };
 class Hero extends GameObjects.Sprite {
@@ -37,7 +39,7 @@ class Hero extends GameObjects.Sprite {
     // Set movement velocity limits
     const { drag, max } = _velocity;
     this.body.setMaxVelocity(max.x, max.y);
-    // this.body.setDragX(drag);
+    this.body.setDragX(drag);
   }
 
   initScene() {
@@ -54,7 +56,7 @@ class Hero extends GameObjects.Sprite {
 
     if (this.cursorKeys.left.isDown) {
       // this.body.setVelocityX(_velocity.left);
-      this.body.setAccelerationX(_velocity.accelRate * -1);
+      this.body.setAccelerationX(-_velocity.accelRate);
       this.body.offset.x = 8; // Center collision boundry with offset
       this.setFlipX(true);
     }
@@ -69,7 +71,36 @@ class Hero extends GameObjects.Sprite {
 
     //
     else {
-      this.body.setVelocityX(0);
+      this.body.setAccelerationX(0);
+    }
+
+    const [onFloor, velocityY, up, space] = [
+      this.body.onFloor(),
+      this.body.velocity.y,
+      this.cursorKeys.up,
+      this.cursorKeys.space,
+    ];
+
+    const isJumping = up.isDown || space.isDown;
+    const didPressJump =
+      Input.Keyboard.JustDown(this.cursorKeys.up) ||
+      Input.Keyboard.JustDown(this.cursorKeys.space);
+
+    // Method 1: Produces bounce effect if key is held down
+    // if (isJumping && onFloor) {
+    //   this.body.setVelocityY(-_velocity.jump);
+    // }
+
+    // Method 2: Produces one jump per press (while onFloor)
+    if (didPressJump && this.body.onFloor()) {
+      this.body.setVelocityY(-_velocity.jump);
+    }
+
+    // # Deceleration
+    // When jump is pressed and released before velocityY reaches 
+    // its max (a negative value), reset velocityY to decelerate jumping
+    if (!isJumping && velocityY < -_velocity.decelRate) {
+      this.body.setVelocityY(-_velocity.decelRate);
     }
   }
 }
